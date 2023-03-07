738e570e5963052955e265bfbe364f4e4c430b1d
Orgabor <gabor.gazdag@orgabor.com>
2023-03-07 18:26:02 +0100

    added

--- HTML/php/form-process.php
+++ HTML/php/form-process.php
@@ -25,7 +25,7 @@ if (empty($_POST["phone"])) {
 if (empty($_POST["address"])) {
     $errorMSG .= "Address is required ";
 } else {
-    $message = filter_var($_POST["address"], FILTER_SANITIZE_SPECIAL_CHARS);
+    $address = filter_var($_POST["address"], FILTER_SANITIZE_SPECIAL_CHARS);
 }
 
 
